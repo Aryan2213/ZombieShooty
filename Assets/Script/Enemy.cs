105fba91b5ca48b1a395f6debce331d71ff404d8
threepat1 <42788704+threepat1@users.noreply.github.com>
2019-05-24 16:03:33 +1000

    Add UI in game

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace ZombieShooty
+{
+
+    public class Enemy : MonoBehaviour
+    {
+
+        public int health = 100;
+        public int damage = 25;
+
+        public float attackDelay = 3;
+        float lastAttacked = -9999;
+
+        //public GameObject player;
+        public Transform target;
+        private NavMeshAgent nav;
+        float targetDistance;
+
+        // ------------------------------------------------- //
+
+        public void DealDamage(int amount)
+        {
+            // Deal DMG
+            health -= amount;
+
+            // Dead
+            if (health <= 0)
+                Destroy(gameObject);
+        }
+
+
+        // ------------------------------------------------- //
+        public void Start()
+        {
+            nav = GetComponent<NavMeshAgent>();
+        }
+
+
+        void Update()
+        {
+            if (targetDistance < 30f)
+            {
+                nav.SetDestination(target.position);
+            }
+
+            targetDistance = Vector3.Distance(target.position, transform.position);
+            if (targetDistance < 1.4f)
+            {
+                Attack();
+            }
+        }
+        public void Attack()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+            {
+                if (hit.collider.gameObject.tag == "Player")
+                {
+
+                    if (Time.time > lastAttacked + attackDelay)
+                    {
+
+                        //if (player.health > 0)
+                        //{
+                        //    player.TakeDamage(10); //make player take damage    
+                        //}
+                        //lastAttacked = Time.time;
+                    }
+                }
+            }
+
+
+
+
+        }
+    }
+}
