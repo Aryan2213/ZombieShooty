bd684b43f6598f146489ac657d0d79e6e7f39ad8
threepat1 <42788704+threepat1@users.noreply.github.com>
2019-06-13 01:07:19 +1000

    update again

--- Assets/Script/SpawnEnemy.cs
+++ Assets/Script/SpawnEnemy.cs
@@ -18,13 +18,13 @@ public class SpawnEnemy : MonoBehaviour
 
     IEnumerator Spawn()
     {
-        while (enemyCount < 30)
+        while (enemyCount < 60)
         {
             posX = Random.Range(-100, 200);
             posZ = Random.Range(-100, 140);
             Vector3 enemyPoint = new Vector3(posX, -52, posZ);
             GameObject clone = Instantiate(enemy, enemyPoint, Quaternion.identity);
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(1f);
             enemyCount += 1;
             if(enemy.name == "Enemy")
             {
@@ -34,13 +34,13 @@ public class SpawnEnemy : MonoBehaviour
     }
     IEnumerator SpawnDog()
     {
-        while (dogCount < 2)
+        while (dogCount < 20)
         {
             posX = Random.Range(-100, 200);
             posZ = Random.Range(-100, 140);
             Vector3 enemyPoint = new Vector3(posX, -52, posZ);
             GameObject clone = Instantiate(dog, enemyPoint, Quaternion.identity);
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(10f);
             dogCount += 1;
             if (dog.name == "Enemy")
             {
