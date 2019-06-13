d42fa8c3c285c0203d746fbda64b343daa0aff74
zacharytafe <36681233+zacharytafe@users.noreply.github.com>
2019-06-13 16:24:11 +1000

    Added flamethrower.

---
+++ Assets/Scripts/Weapons/FlameThrower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameThrower : MonoBehaviour
+{
+    public GameObject fireBullet;
+    public float fireSpeed;   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GameObject insBullet = Instantiate(fireBullet, transform.position, Quaternion.identity) as GameObject;
+            Rigidbody insBulletRig = insBullet.GetComponent<Rigidbody>();
+            insBulletRig.AddForce(transform.right * fireSpeed);
+
+        }
+    }
+}
